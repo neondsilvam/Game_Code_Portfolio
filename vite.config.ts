9827b2b476dfb4a38eae92732b302ec6d51bdc83
Julian D Silva <neon.d.silvam@gmail.com>
2026-06-27 00:25:37 -0700

    updated link

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Code_Portfolio/",
+  base: "/Game_Code_Portfolio/",
   plugins: [react()],
 })
